3e17047d3b7b27d22b93d831d07eb6bae160f135
Spencer Stewart (SS) <sstewart@alterramtnco.com>
2020-05-28 15:07:05 -0700

    Initial commit

---
+++ PubMod.psm1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+    Publish a module to user module directory and optionall to repo.
+
+.DESCRIPTION
+    Copies over the current directory (if not otherwise specified) and then prompts whether to publish the module to a repository.
+    
+.PARAMETER Name
+    Optional. The Name of the module. Otherwise uses the name of the parent directory.
+
+.PARAMETER Path
+    Optional. The path to the module / folder you want to publish.
+
+.PARAMETER Repo
+    Optional. The name of the repository to publish to.
+
+.EXAMPLE
+    PubMod # Copies everything in the current directory to the user module directory, prompts to publish
+
+.AUTHOR
+    Spencer Stewart, Big Bear Mountain Resort
+
+.DATE
+    Created: 2020-4-15
+    Last modified: 2020-5-28
+#>
+function PubMod {
+    param (
+        [Parameter(Mandatory=$false)]
+        [String]
+        $Name,
+        [Parameter(Mandatory=$false)]
+        [String]
+        $Path,
+        [Parameter(Mandatory=$false)]
+        [String]
+        $Repo = "BbmrRepository"
+    )
+
+    if (!$Path) {
+        $Path = Get-Location | Select-Object -ExpandProperty Path
+        Write-Warning "Assuming module to publish is in current directory: $Path"
+    }
+
+    if (!$Name) {
+        $Name = ($Path -split "\\")[-1]
+        Write-Warning "Using name of current directory as module name: $Name"
+        Start-Sleep 1
+    }
+
+    $UserModuleDirectory = $env:PSModulePath -split ';' | Where {$_ -like '*Users*'}
+
+    Write-Output "Copying $Path to $UserModuleDirectory"
+    Start-Sleep -Milliseconds 500
+    Copy-Item $Path $UserModuleDirectory -Recurse -Force
+
+    $Publish = Read-Host "Publish Module to Repo: $Repo [Y | N]"
+    if ($Publish -eq "Y") {
+        Write-Output "Publishing module to $Repo"
+        Start-Sleep -Milliseconds 500
+        Publish-Module -Name $Name -Repository $Repo -Verbose
+    }
+    
+
+}
+
